645081caf410dbd7a16713d77b84d3280d4efe8a
Hector Gonzalez <gorozco.hector@gmail.com>
2022-07-10 17:23:42 -0400

    Create function that changes pixels' colors

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,28 @@
 const sketchContainer = document.querySelector('.sketch-container')
 
 //size of each side of the sketch
-const size = 16;
+const initialSize = 16;
 
 //create a size*size array of pixels 
-for (let i =0; i < (size * size); i++) {
-    const pixel = document.createElement('div')
-    sketchContainer.appendChild(pixel)
-    pixel.classList.add('pixel')
-    //make the size of each pixel enough to fit the container div size
-    pixel.style.cssText = `
-    flex-basis: ${100/size}%;
-    height: ${100/size}%;
-    width: ${100/size}%;`
-    
+function generatePixels(size = initialSize) {
+    for (let i = 0; i < (size * size); i++) {
+        const pixel = document.createElement('div')
+        sketchContainer.appendChild(pixel)
+        pixel.classList.add('pixel')
+        //make the size of each pixel enough to fit the container div size
+        pixel.style.cssText = `
+    flex-basis: ${100 / size}%;
+    height: ${100 / size}%;
+    width: ${100 / size}%;`
+
+    }
 }
 
-const pixels = document.querySelectorAll('.pixels')
+generatePixels()
+
+const pixels = document.querySelectorAll('.pixel')
+
+//change color when hovering over pixels
+pixels.forEach(pixel => pixel.addEventListener('mouseover', (event) => {
+    console.log(event.target.style.backgroundColor='white')
+}))
