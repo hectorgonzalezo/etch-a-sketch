ac581e7e13b4a5b0bb1594d8c88a023e51a92fa6
Hector Gonzalez <gorozco.hector@gmail.com>
2022-07-10 18:40:57 -0400

    Make every button incrementally darker on each pass

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,20 @@ const restartButton = document.querySelector('#restart-button')
 //size of each side of the sketch
 const initialSize = 16;
 let pixels
+let pixelsBrightness //used to incrementally change the color to black
+
+
+//returns a random rgb color, with opacity to fade it to black
+function randomRGB() {
+    let valueR = Math.floor(Math.random() * 127);
+    let valueG = Math.floor(Math.random() * 127);
+    let valueB = Math.floor(Math.random() * 127);
+    return `rgba(${valueR}, ${valueG}, ${valueB})`
+}
 
 //create a size*size array of pixels 
 function generatePixels(size = initialSize) {
+    pixelsBrightness = []
     for (let i = 0; i < (size * size); i++) {
         const pixel = document.createElement('div')
         sketchContainer.appendChild(pixel)
@@ -15,8 +26,9 @@ function generatePixels(size = initialSize) {
         pixel.style.cssText = `
     flex-basis: ${100 / size}%;
     height: ${100 / size}%;
-    width: ${100 / size}%;`
+    width: ${100 / size}%;`;
 
+        pixelsBrightness.push(0);//restart brightness array to 100. 
     };
 
     pixels = document.querySelectorAll('.pixel');
@@ -25,8 +37,17 @@ function generatePixels(size = initialSize) {
 
 //change color when hovering over pixels
 function addPixelColorChange(pixels = pixels) {
-    pixels.forEach(pixel => pixel.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'white'
+    pixels.forEach((pixel, i) => pixel.addEventListener('mouseover', (event) => {
+        if (pixelsBrightness[i] == 0) { //if button is color for the first time
+            event.target.style.backgroundColor = randomRGB();
+            event.target.style.filter = `brightness(100%)`;
+            pixelsBrightness[i] = 100;
+        } else {
+            //remove 10% brightness
+            pixelsBrightness[i] -= 10;
+            event.target.style.filter = `brightness(${pixelsBrightness[i]}%)`
+            console.log(event.target.style.filter)
+        }
     }))
 }
 
